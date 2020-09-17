fbcac685ac199f543c225ebc9bce0322b46f514c
JakeeDesu <hamzamhindate@gmail.com>
2020-09-17 23:09:57 +0100

    responsive components

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bb9e9caf4456083b7efc";
+/******/ 	var hotCurrentHash = "6bb30cccf321cd09daf5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
